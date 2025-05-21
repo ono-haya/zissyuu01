13026d1b12bfd473d2cdcbd02b74681471e07580
ono-haya <s2312990@u.tsukuba.ac.jp>
2025-05-21 14:04:30 +0900

    we created new branch, named csv_test

--- test.py
+++ test.py
@@ -3,6 +3,4 @@ import pandas as pd
 
 data_path = "./data"
 
-df = pd.read_csv("C:/Users/onoha/zissyuu01/data/SIR202312990(4).csv")
-
-print(df)
+df = pd.read_csv(os.path.join(data_path,"SIRS202312990 (4).csv"))
