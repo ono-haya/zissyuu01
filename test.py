86b11fdfd77fb1f5c34dbe43a7c5858a1736d625
kenta499 <be_kenta@icloud.com>
2025-05-21 15:02:08 +0900

    ローカルの変更をコミット

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 data_path = "./data"
 
-df = pd.read_csv("C:/Users/onoha/zissyuu01/data/SIR202312990(4).csv")
+df = pd.read_csv(os.path.join(data_path,"SIR202312990 (4).csv"))
 
 print(df)
