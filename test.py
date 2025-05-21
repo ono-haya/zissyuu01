ebe10d3fb334285bdcd9df741354c089aaebcc54
ono-haya <s2312990@u.tsukuba.ac.jp>
2025-05-21 15:00:55 +0900

    big csv file added

--- test.py
+++ test.py
@@ -4,3 +4,13 @@ import pandas as pd
 data_path = "./data"
 
 df = pd.read_csv(os.path.join(data_path,"SIRS202312990 (4).csv"))
+
+hh = pd.read_csv(os.path.join(data_path,"kdb_2025--ja (1).csv"))
+
+print(hh)
+
+#for i in df:
+ #   for n in df[i]:
+  #      if n == "情報メディア入門":
+   #         print(n)
+    # print(df[i])
