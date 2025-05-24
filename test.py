ecc5d7669d58bb8e9a5bc0b12ddb09b5967b09aa
ono-haya <s2312990@u.tsukuba.ac.jp>
2025-05-24 09:01:27 +0900

    test.pyでcsvファイルを転置してリストに保存

--- test.py
+++ test.py
@@ -5,12 +5,21 @@ data_path = "./data"
 
 df = pd.read_csv(os.path.join(data_path,"SIRS202312990 (4).csv"))
 
-hh = pd.read_csv(os.path.join(data_path,"kdb_2025--ja (1).csv"))
+# hh = pd.read_csv(os.path.join(data_path,"kdb_2025--ja (1).csv"))
 
-print(hh)
+df_transposed = df.T
 
-#for i in df:
- #   for n in df[i]:
-  #      if n == "情報メディア入門":
-   #         print(n)
-    # print(df[i])
+big_file = []
+
+for i in df_transposed:
+    big_file.append([])
+    for j in df_transposed[i]:
+        big_file[i].append(j)
+
+x = []
+
+for i in big_file:
+    if i[7] == "履修中":
+        x.append(i[3])
+
+print(x)
