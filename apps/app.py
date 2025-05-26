be645313883ac09d08559c67ab31eaa8a7699161
apprabbitdev <134241174+apprabbitdev@users.noreply.github.com>
2025-05-26 16:47:24 +0900

    メモ等

--- apps/app.py
+++ apps/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
-
+#名前、学籍番号の登録、データの送信、送信データと単位要件の照合(卒業要件、資格要件←トグルで管理？)送信情報の確認　個人情報　ホーム兼登録、送信画面と確認画面で分ける
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("home.html")
+
+
 
 @app.route("/test")
 def test():
@@ -13,3 +15,4 @@ def test():
 @app.route("/render/<arg>")
 def render(arg):
     return ""
+
