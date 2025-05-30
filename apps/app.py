77e055c2a86680dcf3890b008d9ac107ff06306e
齋藤 洋人 <134241174+apprabbitdev@users.noreply.github.com>
2025-05-30 17:02:29 +0900

    フロントエンド処理の一部

--- apps/app.py
+++ apps/app.py
@@ -1,18 +1,46 @@
-from flask import Flask, render_template
-#名前、学籍番号の登録、データの送信、送信データと単位要件の照合(卒業要件、資格要件←トグルで管理？)送信情報の確認　個人情報　ホーム兼登録、送信画面と確認画面で分ける
+from flask import Flask, session, render_template, request
+import csv
+from flask_session import Session
+
 app = Flask(__name__)
 
+app.config["SESSION_TYPE"] = "filesystem"
+#apps直下にflask_sessionが自動的に作成され、保存される
+Session(app)
+
+#ページごとの要素数は20とする
+per_page = 20
+
 @app.route("/")
-def main():
+def main(initialize = True):    
     return render_template("home.html")
 
+@app.route("/result", methods=["GET", "POST"])
+def get_result():
+    #フォームの情報(ファイル、専攻)を取得
+    file = request.files.get("file")
+    major = request.form.get("major")
+    #専攻の情報をセッションに保存
+    session["major"] = major
+    #処理を行う
+    #処理の結果をセッションに保存
+    
+    return show_result()
 
+#キャッシュ内にあるデータを取得して結果画面を表示する
+@app.route("/result/<int:current_page>")
+def show_result(current_page = 1):
+    #この数字はサンプル
+    total = 84
+    
+    total_pages = (total + per_page - 1) // per_page
+    index_start = (current_page - 1) * per_page
+    index_end = index_start + per_page
+    return render_template("result.html",
+                           current_page=current_page,
+                           total_pages=total_pages,
+                           total=total)
 
-@app.route("/test")
-def test():
-    return "<p>this is app test</p>"
+#引数のCSVをデータ処理部分に送る
 
-@app.route("/render/<arg>")
-def render(arg):
-    return ""
-
+#帰ってきた配列を用いて結果の表示をする
