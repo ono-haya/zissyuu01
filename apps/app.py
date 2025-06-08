c9c847b9e10fd5007ade04cb122bf854fcb0862c
齋藤 洋人 <134241174+apprabbitdev@users.noreply.github.com>
2025-06-08 19:03:35 +0900

    結果をHTMLに変換する処理

--- apps/app.py
+++ apps/app.py
@@ -3,6 +3,7 @@ import csv
 from flask_session import Session
 from apps.check_credit_copy import get_credit
 
+
 app = Flask(__name__)
 
 app.config["SESSION_TYPE"] = "filesystem"
@@ -28,8 +29,12 @@ def get_result():
     #処理を行う
     #マッチングの結果を保存する（不足単位の結果、教科の結果）
     session["currentstate"] = get_credit(file, major)
-
-
+    session["matchresult"] = [
+        ["順位", "科目番号", "科目名", "学期", "時限", "適合度"],  # ←スキーマ行
+        ["1", "GE70101", "知識情報システム実習A", "春", "1", "90"],
+        ["2", "GE70102", "知識情報システム実習B", "秋", "2", "85"],
+        ["3", "GE70103", "知識情報システム実習C", "春", "3", "80"]
+    ]
     #処理の結果をセッションに保存
     #結果のビジュアルを呼び出す
     return show_result()
@@ -37,11 +42,20 @@ def get_result():
 #キャッシュ内にあるデータを取得して結果画面を表示する
 @app.route("/result/<int:current_page>")
 def show_result():
-    #この数字はサンプル
-    total = 84
     print("セッションの中身", session["currentstate"])
+    resutList = session["matchresult"]
+    resultHTML = ""
+    for i in range(len(resutList)):
+        if i == 0:
+            resultHTML += "<tr>" + "".join(f"<th>{attr}</th>" for attr in resutList[i]) + "</tr>"
+        else:
+            resultHTML += "<tr>" + "".join(f"<td>{attr}</td>" for attr in resutList[i]) + "</tr>"
     
-    return render_template("result.html",major = session["major"], currentstate = session["currentstate"] )
+    #返ってきた結果を表示できる状態にする
+    for i in range(per_page):
+        continue
+    
+    return render_template("result.html",major = session["major"], currentstate = session["currentstate"], matchresult = resultHTML)
 
 #引数のCSVをデータ処理部分に送る
 
