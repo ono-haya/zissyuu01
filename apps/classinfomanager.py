0ffb58fccc5c1f7957e985c8067a3e325dc77674
齋藤 洋人 <134241174+apprabbitdev@users.noreply.github.com>
2025-06-09 18:10:52 +0900

    あ

--- apps/classinfomanager.py
+++ apps/classinfomanager.py
@@ -1,9 +1,20 @@
 import os
 import pandas as pd
 path = __path__
-df = pd.read_csv(file, encoding='cp932')
-df.columns = df.columns.str.strip()
+
+
 #引数：ranking[[自主専攻のランキング([科目番号, 一致度],[科目番号, 一致度])],[多種専攻のランキング([科目番号, 一致度],[科目番号, 一致度])]]
-def get_classinfo(ranking):
+def get_classinfo(rankings):
+    df = pd.read_csv("./kdb.csv", encoding='cp932')
+    df.columns = df.columns.str.strip()
+    schema = ["科目番号","科目名","単位数","実施学期","曜時限","授業概要"]
+    result = [[]]
+    for ranking in rankings:
+        for element in ranking:
+            number = element[0]
+            osusume = element[1]
+            element_info = df[df["科目番号"] == number]
+            
+            
 
     return
